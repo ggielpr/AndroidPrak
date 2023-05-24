ecf7aaaf716c47cadaf0a99ce44376e5db92f048
Izabella <125027467+ggielpr@users.noreply.github.com>
2023-05-24 19:26:38 +0300

    lesson7

---
+++ Lesson7/httpurlconnection/src/main/java/ru/mirea/tsitserid/httpurlconnection/MainActivity.java
@@ -0,0 +1,172 @@
+package ru.mirea.tsitserid.httpurlconnection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import ru.mirea.tsitserid.httpurlconnection.databinding.ActivityMainBinding;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private String latitude;
+    private String longitude;
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectivityManager connectivityManager =
+                        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkinfo = null;
+                if (connectivityManager != null) {
+                    networkinfo = connectivityManager.getActiveNetworkInfo();
+                }
+                if (networkinfo != null && networkinfo.isConnected()) {
+                    new DownloadPageTask().execute("https://ipinfo.io/json"); // запуск нового потока
+                } else {
+                    Toast.makeText(getApplicationContext(), "Нет интернета", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+    }
+
+    private class DownloadPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            binding.textIp.setText("Загружаем...");
+            binding.textCity.setText("Загружаем...");
+            binding.textRegion.setText("Загружаем...");
+            binding.textLocation.setText("Загружаем...");
+            binding.textTemperature.setText("Загружаем...");
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadIpInfo(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(MainActivity.class.getSimpleName(), result);
+            try {
+                JSONObject responseJson = new JSONObject(result);
+                Log.d(MainActivity.class.getSimpleName(), "Response: " + responseJson);
+                String ip = responseJson.getString("ip");
+                binding.textIp.setText("IP адрес: " + ip);
+                String city = responseJson.getString("city");
+                binding.textCity.setText("Город: " + city);
+                String region = responseJson.getString("region");
+                binding.textRegion.setText("Регион: " + region);
+                String location = responseJson.getString("loc");
+                binding.textLocation.setText("Местоположение: " + location);
+                latitude = location.substring(0,location.indexOf(","));
+                longitude = location.substring(location.indexOf(",")+1);
+                new WeatherInfo().execute("https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&current_weather=true");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(result);
+        }
+    }
+
+    public String downloadIpInfo(String address) throws IOException {
+        InputStream inputStream = null;
+        String data = "";
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(100000);
+            connection.setConnectTimeout(100000);
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(true);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 200 OK
+                inputStream = connection.getInputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int read = 0;
+                while ((read = inputStream.read()) != -1) {
+                    bos.write(read);
+                }
+                bos.close();
+                data = bos.toString();
+            } else {
+                data = connection.getResponseMessage() + ". Error Code: " + responseCode;
+            }
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return data;
+    }
+
+    private class WeatherInfo extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadIpInfo(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(MainActivity.class.getSimpleName(), result);
+            try {
+                JSONObject responseJson = new JSONObject(result);
+                Log.d(MainActivity.class.getSimpleName(), "Response: " + responseJson);
+                JSONObject weatherJson = responseJson.getJSONObject("current_weather");
+                String temp = weatherJson.getString("temperature");
+                binding.textTemperature.setText("Температура:" + temp);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(result);
+        }
+    }
+}
---
+++ Lesson7/timeservice/src/main/java/ru/mirea/tsitserid/timeservice/MainActivity.java
@@ -0,0 +1,60 @@
+package ru.mirea.tsitserid.timeservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private TextView mTextView;
+    private final String host = "time-b.nist.gov"; // или time-a.nist.gov or time-b.nist.gov
+    private final int port = 13;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView);
+    }
+
+    public void onClick(View view) {
+        GetTimeTask timeTask = new GetTimeTask();
+        timeTask.execute();
+    }
+
+    private class GetTimeTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            String timeResult = "";
+            try {
+                Socket socket = new Socket(host, port);
+                BufferedReader reader = SocketUtils.getReader(socket);
+                reader.readLine();
+                timeResult = reader.readLine();
+                Log.d(TAG, timeResult);
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return timeResult;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            String onlyDateAndTime = "";
+            onlyDateAndTime = result.substring(5,23);
+            mTextView.setText(onlyDateAndTime);
+        }
+    }
+}
